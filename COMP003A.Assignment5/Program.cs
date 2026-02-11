635d2ab1d71399469a8068bc3acabd3201318acb
JohnVisual <johnpatrickdeguzman149@gmail.com>
2026-02-11 15:28:18 -0800

    Added a loop to repeat the program based on the users choice

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -6,6 +6,32 @@
         {
             DisplayIntroduction();
 
+            bool runAgain = true;
+
+            while (runAgain)
+            {
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter a whole number:");
+                int number = int.Parse(Console.ReadLine());
+
+                DisplayUserInput(name, number);
+
+                int result = CalculateResult(number);
+
+                Console.WriteLine("Calculated result: " + result);
+
+                Console.Write("Would you like to run the program again? (y/n): ");
+                string answer = Console.ReadLine();
+
+                runAgain = (answer == "y" || answer == "Y");
+                Console.WriteLine();
+
+                Console.WriteLine("Program ended.");
+
+            }
+
             static void DisplayIntroduction()
             {
                 Console.WriteLine("Welcome to the Program Organizer!");
