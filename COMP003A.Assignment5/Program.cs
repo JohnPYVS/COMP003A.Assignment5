8b94e8a9656829e415d97a4b8cdd32a7e01e2274
JohnVisual <johnpatrickdeguzman149@gmail.com>
2026-02-11 16:40:26 -0800

    Added comments

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -4,24 +4,32 @@
     {
         static void Main(string[] args)
         {
-            DisplayIntroduction();
+           // Why methods exist is to make the program look organize and to make the steps look clear
+           // Methods uses parameters and return values while Main controls the program
+            
+            DisplayIntroduction(); 
 
             bool runAgain = true;
 
+            // The loop depends on the user input
             while (runAgain)
             {
+              // This must be read in Main
                 Console.Write("Enter your name: ");
                 string name = Console.ReadLine();
 
                 Console.Write("Enter a number: ");
                 int number = int.Parse(Console.ReadLine());
 
+                // Passed values to a method using parameters
                 DisplayUserInput(name, number);
 
+                // Stores returned value in Main 
                 int result = CalculateResult(number);
 
                 Console.WriteLine("Calculated result: " + result);
 
+                // Returned value in Main for decision making
                 if (result > 10)
                 {
                     Console.WriteLine("The result is greater than 10.");
@@ -41,19 +49,24 @@
 
             Console.WriteLine("Program ended.");
             
+            // This shows the program introduction message
+            // It only prints its own message
             static void DisplayIntroduction()
             {
                 Console.WriteLine("Welcome to the Program Organizer!");
                 Console.WriteLine();
             }
 
+            // This is to pass multiple values into a method using parameters
+            // Only parameters, and not variables from Main
             static void DisplayUserInput(string userName, int userNumber)
             {
                 Console.WriteLine();
                 Console.WriteLine("You entered Name: " + userName + ", Number: " + userNumber);
                 Console.WriteLine();
             }
-
+            
+            // This is to calculate using the numeric value and to retun the result
             static int CalculateResult(int value)
             {
                 int calculated = value * 2;
